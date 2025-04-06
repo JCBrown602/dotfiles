3af9b94ff86fbefa7b6585512449b00d0a250651
Jason Brown <3117301+JCBrown602@users.noreply.github.com>
2025-04-06 15:59:07 -0500

    Comments to show which dotfiles & links are added

--- setup.sh
+++ setup.sh
@@ -2,10 +2,19 @@
 
 echo "Setting up symlinks..."
 
+echo "Adding: ln -sf ~/dotfiles/.zshrc ~/.zshrc"
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+echo "Adding: ln -sf ~/dotfiles/.vimrc ~/.vimrc"
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+
+echo "Adding: ln -sf ~/dotfiles/.gitconfig ~/.gitconfig"
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+
+echo "Adding: ln -sf ~/dotfiles/.aliases ~/.aliases"
 ln -sf ~/dotfiles/.aliases ~/.aliases
+
+echo "Adding: ln -sf ~/dotfiles/.zprofile ~/.zprofile"
 ln -sf ~/dotfiles/.zprofile ~/.zprofile
 
 echo "dotfiles setup complete. ✅"
